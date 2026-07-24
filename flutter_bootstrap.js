c8ebc2cd54495b70bd9a6b0a12ce6986642fdd25
Lans12345 <olanalans12345@gmail.com>
2026-07-25 06:17:26 +0800

    updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1229686363"
+    serviceWorkerVersion: "69703616"
   }
 });
